0a9e4b7ecc58fae57004a375e5c5829e287931bf
Migs9 <galleuk@gmail.com>
2023-05-05 12:16:45 +0100

    delete func update

--- src/utils/index.js
+++ src/utils/index.js
@@ -151,6 +151,52 @@ export const deleteActiveTodo = async ( todo, jwtToken ) => {
     }
 };
 
+export const deleteDoneTodo = async ( todo, jwtToken ) => {
+    try {
+        const response = await fetch("http://localhost:5001/donetodos/deletedonetodo", {
+            method:"DELETE",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${jwtToken}`
+            },
+            body: JSON.stringify({
+                "todo": todo
+            })
+        });
+        if (response.status === 401) {
+            const notAuthorized = {
+                message: "User not authorized"
+            };
+            throw new Error(notAuthorized);
+        }
+        else if (response.status === 404) {
+            const notFound = {
+                message: "Todo not found"
+            };
+            throw new Error(notFound);
+        }
+        else if (!response.ok) {
+            throw new Error("User not Authorized");
+          }
+        const data = await response.json()
+        const successMessage = {
+            message: "success",
+            todo: {
+              id: data.user.todo.id,
+              todo: data.user.todo.todo,
+            },
+        };
+        return successMessage;
+    } catch (error) {
+        const errorMessage = {
+            message: "error",
+            error: error.message,
+        };
+        throw new Error(errorMessage);
+    }
+};
+
 export const LoginPage = async ( username, password, jwtToken ) => {
     try {
         const response = await fetch("http://localhost:5001/users/loginUser", {
